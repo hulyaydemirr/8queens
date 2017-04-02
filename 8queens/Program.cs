7e2c044a5ab3de1b50a349a714a3c358cd7a7a54
Naim Buru <Naim Buru>
2017-04-03 01:56:27 +0300

    initializeBoard() fonksiyonu eklendi

--- 8queens/Program.cs
+++ 8queens/Program.cs
@@ -10,6 +10,38 @@ namespace _8queens
     {
         static void Main(string[] args)
         {
+            bool[,] board= initializeBoard(8); //initialize for 8x8 (8 queens)
+
+
+
+
+        }
+
+        static bool[,] initializeBoard(int size)
+        {
+            bool[,] board = new bool[size, size];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                int randomSquare = rnd.Next(8); //placing queen to a random square in the row
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (j != randomSquare)
+                        board[i, j] = false;
+                    else
+                        board[i, j] = true; 
+                }
+            }
+
+            return board;
+        }
+
+        static int calculateCollisions(bool[,] board)
+        {
+            return 0;
+
         }
     }
 }
