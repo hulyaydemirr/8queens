691747f7e927f96d29ca19ec15df25283a57c58c
Naim Buru <Naim Buru>
2017-04-03 03:54:31 +0300

    calculateCollisions() fonksiyonu eklendi

--- 8queens/Program.cs
+++ 8queens/Program.cs
@@ -10,9 +10,9 @@ namespace _8queens
     {
         static void Main(string[] args)
         {
-            bool[,] board= initializeBoard(8); //initialize for 8x8 (8 queens)
+            bool[,] board = initializeBoard(8); //initialize for 8x8 (8 queens)
 
-
+            Console.WriteLine(calculateCollisions(board));
 
 
         }
@@ -23,15 +23,15 @@ namespace _8queens
             Random rnd = new Random();
 
 
-            for (int i = 0; i < size - 1; i++)
+            for (int i = 0; i < size; i++)
             {
                 int randomSquare = rnd.Next(8); //placing queen to a random square in the row
-                for (int j = 0; j < size - 1; j++)
+                for (int j = 0; j < size; j++)
                 {
                     if (j != randomSquare)
                         board[i, j] = false;
                     else
-                        board[i, j] = true; 
+                        board[i, j] = true;
                 }
             }
 
@@ -40,7 +40,55 @@ namespace _8queens
 
         static int calculateCollisions(bool[,] board)
         {
-            return 0;
+            int size = Convert.ToInt32(Math.Sqrt(board.Length)); //if the board is 8x8, board.Length will be 64
+            int totalCollisions = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (board[i, j] == true) // if there is a queen on that square
+                    {
+                        for (int z = j + 1; z < size; z++) //check the row
+                        {
+                            if (board[i, z] == true)
+                            {
+                                totalCollisions++;
+                                break;
+                            }
+                        }
+
+                        for (int z = i + 1; z < size; z++) //check the column
+                        {
+                            if (board[z, j] == true)
+                            {
+                                totalCollisions++;
+                                break;
+                            }
+                        }
+
+                        for (int z = 1 ; i + z < size && j + z < size; z++) //check the bottom-right diagonal
+                        {
+                            if (board[i + z, j + z] == true)
+                            {
+                                totalCollisions++;
+                                break;
+                            }
+                        }
+
+                        for (int z = 1; i - z >= 0 && j + z < size; z++) //check the top-right diagonal
+                        {
+                            if (board[i - z, j + z] == true)
+                            {
+                                totalCollisions++;
+                                break;
+                            }
+                        }
+
+                    }
+                }
+            }
+            return totalCollisions;
 
         }
     }
